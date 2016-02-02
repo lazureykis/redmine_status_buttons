a8684781a799ae90ab72e9e1aecc9d6d51260f75
Pavel Lazureykis <lazureykis@gmail.com>
2016-02-02 18:50:03 +0300

    UPD plugins renamed

--- init.rb
+++ init.rb
@@ -1,11 +1,11 @@
 require 'status_buttons_hook_listener'
 
-Redmine::Plugin.register :status_buttons do
+Redmine::Plugin.register :redmine_status_buttons do
   name 'Status Buttons'
   author 'Pavel Lazureykis'
   description 'Buttons for changing status without opening edit form.'
   version '0.0.1'
-  url 'https://github.com/lazureykis/status_buttons'
+  url 'https://github.com/lazureykis/redmine_status_buttons'
   author_url 'https://github.com/lazureykis'
 end
 
